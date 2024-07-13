1231b901a3a428f2348a8fccfc274ac48a20856b
TeyUS <teyhanytu@gmail.com>
2024-07-13 13:17:12 +0300

    Initial Commit

---
+++ EterationStudyCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EterationStudyCase
+//
+//  Created by Teyhan Uslu on 13.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
